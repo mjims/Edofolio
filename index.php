c077b935bdf841c92be7035bb7d722dff4918537
mjims <denlamjimsedo@gmail.com>
2023-06-06 10:42:28 +0100

    Message de commit

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
+	<title>Mjims Denla</title>
+	<style type="text/css">
+		.imgP{
+			height: 140px;
+			width: 140px;
+			border-radius: 100%;
+			background-image: url(mjims.png);
+			background-size: 100% 170%;
+			background-origin: t;
+			background-position-y: 65%;
+			background-position-x: 80%;
+		}
+		.row{
+			margin-top: 20px;
+			margin-bottom: 30px;
+		}
+		.bi{
+			margin-right: 20px;
+			font-size: 30px;
+		}
+	</style>
+</head>
+<body class="container">
+	<div class="ind-sect1 row">
+		<div class="col-4">
+			<div class="imgP" title="Édouard Denla"></div>			
+		</div>
+		<div class="col-7">
+			<h1><strong><i>Édouard DENLA</i></strong></h1>
+			Développeur Full-Stack  dépuis Bénin <br>
+			<span><a href="https://www.facebook.com/edouard.mjimsdenla" target="_blank"><i class="bi bi-facebook"></i></a></span>
+			<span><a href="https://twitter.com/mjims" target="_blank"><i class="bi bi-twitter"></i></a></span>
+			<span><a href="https://wa.me//22967697667" target="_blank" style="color: #52d33e;"><i class="bi bi-whatsapp"></i></a></span></i>
+			<span><a href="https://github.com/mjims" target="_blank" style="color: black;"><i class="bi bi-github"></i></a></span></i>
+		</div>
+	</div>
+	<div class="ind-sect2 row">
+		<div class="col-md-4 col-10">
+			À PROPOS DE MOI
+		</div>
+		<div class="col-md-7 col-10">
+			Je suis un ingénieur full-stack motivé qui a de l'expérience dans la création et le développement d'applications et de services web dans différents domaines. Je suis également passionné par les contributions en open source, le travail dans des environnements à fort impact et la contribution à la croissance des équipes de développement en aidant à construire de grands produits.
+		</div>
+	</div>	
+	<div class="ind-sect2 row">
+		<div class="col-md-4 col-10">COMPETENCES</div>
+		<div class="col-md-7 col-10">
+			<h3>LANGUAGES ET FRAMWORKS</h3>
+			<p>
+				<ul>
+					<li>HTML 5 & CSS 3</li>
+					<li>Bootstrap</li>
+					<li>Mysql</li>
+					<li>PHP</li>
+					<li>JavaScript</li>
+					<li>JQuery</li>
+					<li>C</li>
+					<li>C++</li>
+					<li>Python</li>
+				</ul>
+			</p>
+		</div>
+	</div>
+	<div class="ind-sect2 row">
+		<div class="col-md-4 col-10">PROJETS</div>
+		<div class="col-md-7 col-10">
+			<h3><a href="" target="blank">E-commerce Pâtisserie</a></h3>
+			<p>UN projet dévéloppé pour Bouche-Fine une entreprise beninoise de pâtisserie <br>
+				Le projet a été constriut nativement avec HTML, CSS, Bootstrap, PHP, JavaScript, JQuery et Ajax;
+				J'ai également utilisé l'API de payement mobile pour gérer le payement automatique. 
+			</p>
+			<h3><a target="blank" href="https://github.com/mjims/MjimsPat.git">PanierShop</a></h3>
+			<p>PanierShop est un framwork JavaScript que j'ai réalialisé pour permettre à tous les programmeurs de gérer très facilement le panier sur leur projet de E-commerce. <br>C'est un framwork entièrement gratuit disponible sur <a title="Cliquez sur consulter le projet!" target="blank" href="https://github.com/mjims/MjimsPat.git">GitHub</a></p>
+
+		</div>
+	</div>
+	<div class="ind-sect2 row">
+		<div class="col-md-4 col-10">
+			À PROPOS DE MOI
+		</div>
+		<div class="col-md-7 col-10">
+			
+		</div>
+	</div>
+	<div class="ind-sect1"></div>
+	<div class="ind-sect1"></div>
+
+</body>
+</html>
