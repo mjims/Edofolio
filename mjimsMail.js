86338805e344bca4fd9f42309f48bad445c577ff
mjims <134434029+mjims@users.noreply.github.com>
2023-07-08 20:20:23 +0100

    Create mjimsMail.js
    For sending mail and generate code

---
+++ mjimsMail.js
@@ -0,0 +1,26 @@
+<script>
+function sendEmail(sender,dest,sujet,corps,pass) {
+  	Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "karljdemfond@gmail.com",
+    Password: pass,
+    To: dest,
+    From: sender,
+    Subject: sujet,
+    Body: corps
+  }).then(
+    message => alert(message)
+  );
+}
+function codegenerer() {
+	const caraters='ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz';
+	let code='';
+	for (var i = 0; i < 4; i++) {
+		const randomIndex=Math.floor(Math.random()* caraters.length);
+		code+=caraters.charAt(randomIndex);
+	}
+	const timestamp=Date.now();
+	code+='DEM'+timestamp;
+	return code;
+}
+</script>
