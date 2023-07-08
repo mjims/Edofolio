3b8b0a06a86741fd846a13b0e1b1b29fd091feaa
mjims <134434029+mjims@users.noreply.github.com>
2023-07-08 20:34:55 +0100

    Update mjimsMail.js

--- mjimsMail.js
+++ mjimsMail.js
@@ -1,4 +1,3 @@
-<script>
 function sendEmail(sender,dest,sujet,corps,pass) {
   	Email.send({
     Host: "smtp.elasticemail.com",
@@ -12,7 +11,7 @@ function sendEmail(sender,dest,sujet,corps,pass) {
     message => alert(message)
   );
 }
-function codegenerer() {
+function codegenerer(){
 	const caraters='ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz';
 	let code='';
 	for (var i = 0; i < 4; i++) {
@@ -23,4 +22,4 @@ function codegenerer() {
 	code+='DEM'+timestamp;
 	return code;
 }
-</script>
+
